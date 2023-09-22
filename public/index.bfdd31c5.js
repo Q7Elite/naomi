cc8e464d508a233aeaf6bd8d872ad3dc8442b263
Bonface Gathura <karichubonface@yahoo.com>
2023-09-22 22:37:51 +0300

    build .gitignore file

---
+++ public/index.bfdd31c5.js
@@ -0,0 +1,3 @@
+const e=document.querySelector(".nav-toggle"),n=document.querySelectorAll(".nav__link");e.addEventListener("click",()=>{document.body.classList.toggle("nav-open")}),n.forEach(e=>{e.addEventListener("click",()=>{document.body.classList.remove("nav-open")})});//# sourceMappingURL=index.bfdd31c5.js.map
+
+//# sourceMappingURL=index.bfdd31c5.js.map
